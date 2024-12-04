5d1f514720b6b95a5ad7dd5ecb221f9e2732573f
Tanish Azad <tanishazad03@gmail.com>
2024-12-04 18:39:03 +0530

    readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Country Code Picker
 
-![Icon](./assets/icon.png) ![Picker](./assets/picker.png) ![Search](./assets/picker_search.png)
+## Android
+![](./assets/Android_light_closed.png)
+![](./assets/Android_light_drawer.png)
+![](./assets/Android_light_search.png)
+![](./assets/Android_dark_closed.png)
+![](./assets/Android_dark_drawer.png)
+![](./assets/Android_dark_search.png)
+
+## iOS
+![](./assets/IPhone_light_closed.png)
+![](./assets/IPhone_light_drawer.png)
+![](./assets/IPhone_light_search.png)
+![](./assets/IPhone_dark_closed.png)
+![](./assets/IPhone_dark_drawer.png)
+![](./assets/IPhone_dark_search.png)
 
 Country Code Picker for Compose Multiplatform. Supported platforms:-
 - Android
--- demo/src/androidMain/kotlin/com/wannacall/demo/MainActivity.kt
+++ demo/src/androidMain/kotlin/com/wannacall/demo/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.wannacall.demo
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge(SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT))
         setContent {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
